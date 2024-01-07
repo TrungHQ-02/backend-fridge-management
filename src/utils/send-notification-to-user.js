60a4788e3ca63ef0639edb49be284ba1a2210b98
Hoàng Quốc Trung <103867359+TrungHQ-02@users.noreply.github.com>
2024-01-07 09:19:02 +0700

    send notification utils

--- src/utils/send-notification-to-user.js
+++ src/utils/send-notification-to-user.js
@@ -1,48 +1,35 @@
 const getText = require("./lang/get_text.js");
 const errorHelper = require("./helpers/error-helper.js");
 const { getMessaging } = require("firebase-admin/messaging");
+const db = require("../models/index.js");
 
-const sendNotificationToUserId = (userId) => {
-  await;
-  const registrationToken =
-    "ctCKdFawTJ6-2m_gxvDF2k:APA91bGRwVqh52eeGDum8vXNs-ezO5P9aQJfeH-XMUXpa1BwuQBr7C2tcKCm0eM62jyrfsJ6tWKCSm8kY9Yr0UFnGuXMkuYTyyNcihcMCv3ivsrCcDQDKt1smIuHzmhQVYjR9GMGNgr3";
-
-  const message = {
-    notification: {
-      title: "Notif",
-      body: "This is a Test Notification",
+const sendNotificationToUserId = async (userId, message, req, res) => {
+  console.log(userId, message);
+  let user = await db.User.findOne({
+    where: {
+      id: userId,
     },
-    token:
-      "ctCKdFawTJ6-2m_gxvDF2k:APA91bGRwVqh52eeGDum8vXNs-ezO5P9aQJfeH-XMUXpa1BwuQBr7C2tcKCm0eM62jyrfsJ6tWKCSm8kY9Yr0UFnGuXMkuYTyyNcihcMCv3ivsrCcDQDKt1smIuHzmhQVYjR9GMGNgr3",
+  });
+
+  console.log(user.notificationToken);
+
+  let notifToken = user.notificationToken;
+  const sendMessage = {
+    data: message,
+    token: notifToken,
   };
 
   getMessaging()
-    .send(message)
+    .send(sendMessage)
     .then((response) => {
       console.log("Successfully sent message:", response);
     })
     .catch((error) => {
       console.log("Error sending message:", error);
+      errorHelper("00005x", req);
     });
 };
 
-const sendNotificationToListOfUsers = (listOfUsers) => {
-  const registrationTokens = [
-    "YOUR_REGISTRATION_TOKEN_1",
-    // …
-    "YOUR_REGISTRATION_TOKEN_N",
-  ];
-
-  const message = {
-    data: { score: "850", time: "2:45" },
-    tokens: registrationTokens,
-  };
-
-  getMessaging()
-    .sendMulticast(message)
-    .then((response) => {
-      console.log(response.successCount + " messages were sent successfully");
-    });
+module.exports = {
+  sendNotificationToUserId: sendNotificationToUserId,
 };
-
-module.exports = sendNotificationToUserId;
